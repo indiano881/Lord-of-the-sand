4733b03c20e131628c39fac78808ab918c627b92
Davide Baldi <d.baldi88@gmail.com>
2023-12-03 08:28:36 +0100

    add rob js to check

--- javascript.js
+++ javascript.js
@@ -64,7 +64,7 @@ const chooseCharacterSkills = (skillPoints) => {
         userCharacter.life=parseInt(prompt("Point to life "))
         userCharacter.luck=parseInt(prompt("Point to luck "))
     }
-
+//CONTINUARE QUESTO
 }
 
 
@@ -72,6 +72,148 @@ const chooseCharacterSkills = (skillPoints) => {
 
 
 
+
+
+
+chooseCharacterName();
+chooseCharacterElementAndWeapon();
+/*
+const playGame = () => {
+    const characters = ["Fighter", "Ninja", "Wizard", "Amazon", "Necromancer"];
+    const enemies = ["Dragon", "Goblin", "Devil", "Giant", "Demigorgon"];
+
+    let newParty = Array.from(characters)
+
+    while (newParty.length > 1) {
+        newParty.forEach(character => console.log(`This is a brave ${character}`))
+        let deadCharacter = newParty[Math.floor(Math.random() * newParty.length)]
+        const enemy = enemies[Math.floor(Math.random() * enemies.length)]
+
+        const chooseYourWeapon = customer => {
+            let story = `The ${customer} enters the weapon shop and looks at what is available`;
+
+            let weaponOfChoice;
+            if (customer === "Fighter") {
+                weaponOfChoice = "sword";
+            } else if (customer === "Ninja") {
+                weaponOfChoice = "throwing star"
+            } else if (customer === "Wizard") {
+                weaponOfChoice = "staff";
+            } else if (customer === "Amazon") {
+                weaponOfChoice = "javelin"
+            } else {
+                weaponOfChoice = "wand"
+            }
+
+            let choosingAWeapon = `The ${customer} sees a beautiful ${weaponOfChoice}. They pay for it in gold and leave the store`;
+
+            return story + " " + choosingAWeapon;
+        }
+
+        newParty.forEach(character => console.log(chooseYourWeapon(character)))
+
+        const monsterEncounter = enemy => {
+            console.log(`Our brave party run into a ${enemy}! It looks very angry! It attacks!!`)
+        }
+
+        monsterEncounter(enemy);
+
+        newParty.forEach(partyMember => console.log(`${partyMember} attacks the ${enemy}`))
+
+        let characterOutcomes = newParty.map(character => {
+            if (character === deadCharacter) {
+                return `Unfortunately, ${character} was killed`
+            }
+
+            return `${character} has levelled up!`;
+        })
+
+        characterOutcomes.forEach(outcome => console.log(outcome));
+
+        newParty = newParty.filter(partyMember => partyMember !== deadCharacter);
+
+        /*
+        let newParty = [];
+        
+        for (let i = 0; i < characters.length; i ++) {
+            if (characters[i] !== deadCharacter) {
+                newParty.push(characters[i])
+            }
+        }
+
+        newParty.forEach(member => console.log(member));
+    }
+
+    newParty.forEach(winner => console.log(`${winner} is the only survivor! There can be only one.`))
+}
+*/
+/*
+let characters = ["Fighter", "Wizard", "Thief", "Archer"];
+let monsters = ["Dragon", "Giant", "Demon", "Killer Rabbit"]
+let username;
+let characterClass;
+
+const createCharacter = () => {
+    username = prompt("Enter a name", "What should we call you?");
+    characterClass = characters[Math.floor(Math.random() * characters.length)];
+}
+
+const characterIntroduction = () => {
+    createCharacter();
+
+    let body = document.querySelector("body");
+    let characterBio = document.createElement("div");
+    characterBio.innerHTML = `<p>Meet ${username}</p><p>${username} is a badass ${characterClass}</p>`;
+    body.appendChild(characterBio)
+}
+
+const weapon = characterClass => {
+    if (characterClass === "Fighter")
+        return "sword";
+
+    if (characterClass === "Wizard")
+        return "staff";
+
+    if (characterClass === "Thief")
+        return "dagger";
+
+    return "bow"
+}
+
+const characterSkill = characterClass => {
+    let characterWeapon = weapon(characterClass);
+
+    let body = document.querySelector("body");
+    let characterSkillDiv = document.createElement("div");
+    characterSkillDiv.textContent = `${username} has a very nice ${characterWeapon}. They like to use it a lot`;
+    body.appendChild(characterSkillDiv);
+}
+
+const monsterBattle = () => {
+    let monster = monsters[Math.floor(Math.random() * monsters.length)];
+    let roll = Math.floor(Math.random() * 3) + 1;
+
+    let body = document.querySelector("body");
+    let monsterDiv = document.createElement("div");
+    let monsterDescription = document.createElement("p");
+    let monsterFight = document.createElement("p");
+
+    monsterDescription.textContent = `Oh no! ${username} went for a walk and ran into a ${monster}! It looks really angry. It attacks!!!`;
+    let outcome = roll === 1 ? `${username} uses their weapon and kills the fierce ${monster}`
+                             : roll === 2 ? `${username} uses their weapon and the ${monster} runs away` 
+                             : `The ${monster} eats ${username}`;
+    
+    monsterFight.textContent = outcome;
+
+    monsterDiv.appendChild(monsterDescription);
+    monsterDiv.appendChild(monsterFight);
+    body.appendChild(monsterDiv);
+}
+
+characterIntroduction();
+characterSkill(characterClass);
+monsterBattle();
+*/
 
 
 /*
@@ -99,5 +241,3 @@ const createUserCharacter = () => {
 }
 */
 
-chooseCharacterName();
-chooseCharacterElementAndWeapon();
